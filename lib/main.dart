068fa663ab73c8974faef1874cd7c5c39b3247c1
Michał Łukaszewski <mimanek123@wp.pl>
2023-01-27 16:30:48 +0100

    add MaterialApp and HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_example/examples/SQLite%20CRUD%20Example/main.dart';
+import 'package:flutter_example/home_page.dart';
 
-void main() {
+MaterialApp main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
+	
     return const MaterialApp(
       title: 'Flutter Examples',
       debugShowCheckedModeBanner: false,
-      home: FinalView(),
+      home: HomePage(),
     );
   }
-}
-
